39e1e2d4b1c3bedc77d43d2dc5ed21e7d3bc98f8
rcravens <bob.cravens@gmail.com>
2025-10-03 21:43:45 -0500

    wip

--- videos/video_07/http_static.js
+++ videos/video_07/http_static.js
@@ -18,19 +18,18 @@ const MIME_TYPES = {
 
 const STATIC_PATH = path.join(process.cwd(), "./static");
 
-const toBool = [() => true, () => false];
-
+// url examples: '/', '/profile.html', '/about.html'
 const prepareFile = async (url) => {
     const paths = [STATIC_PATH, url];
     if (url.endsWith("/")) paths.push("index.html");
     const filePath = path.join(...paths);
     const pathTraversal = !filePath.startsWith(STATIC_PATH);
-    const exists = await fs.promises.access(filePath).then(...toBool);
+    const exists = await fs.promises.access(filePath).then(() => true, () => false);
     const found = !pathTraversal && exists;
     const streamPath = found ? filePath : `${STATIC_PATH}/404.html`;
     const ext = path.extname(streamPath).substring(1).toLowerCase();
     const stream = fs.createReadStream(streamPath);
-    return { found, ext, stream };
+    return {found, ext, stream};
 };
 
 http
@@ -38,7 +37,7 @@ http
         const file = await prepareFile(req.url);
         const statusCode = file.found ? 200 : 404;
         const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
-        res.writeHead(statusCode, { "Content-Type": mimeType });
+        res.writeHead(statusCode, {"Content-Type": mimeType});
         file.stream.pipe(res);
         console.log(`${req.method} ${req.url} ${statusCode}`);
     })
